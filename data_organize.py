315279d18d1b8671ee2eb91e34ba5e1de428b5cb
Jeanie Chen <chenjeanie29@gmail.com>
2019-08-12 15:28:27 -0400

    fixed August 9th problems, trying 1 iteration

--- data_organize.py
+++ data_organize.py
@@ -120,6 +120,7 @@ if __name__ == "__main__":
     ulg_to_csv = "ulog2csv {}".format(file[0])
     # os.system(ulg_to_csv)
     print(file[0])
+    os.system("pwd")
     file_path = "./build/posix_sitl_default/tmp/rootfs/fs/microsd/log/{}/{}"
     file_path = file_path.format(s, file[0])
     subprocess.call(["ulog2csv", file_path])
